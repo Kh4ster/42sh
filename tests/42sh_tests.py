37daddd826b530c55904e0e57614d9e4d607371f
pierrick.made <pierrick.made@epita.fr>
2019-11-05 23:01:47 +0100

    ADD - 42sh_tests.py - nearly finished, compiling, not tested yet
    Signed-off-by: pierrick.made <pierrick.made@epita.fr>

--- tests/42sh_tests.py
+++ tests/42sh_tests.py
@@ -1,9 +1,64 @@
 from argparse import ArgumentParser
 from pathlib import Path
+import glob
+from difflib import unified_diff
+import subprocess as sp
 
-def launch_tests(binary, args):
-    test_list = args.list
-    print(f"should launch tests on {binary}")
+def run_shell(args, stdin):
+    return sp.run(args, capture_output=True, text=True, input=stdin)
+
+def diff(ref, student):
+    ref = ref.splitlines(keepends=True)
+    student = student.splitlines(keepends=True)
+
+    return ''.join(unified_diff(ref, student, fromfile="ref", tofile="42sh"))
+
+def test(binary, test_case):
+    ref = run_shell(["bash", "--posix"], test_case["stdin"])
+    student = run_shell([binary], test_case["stdin"])
+
+    for check in test_case.get("checks", ["stdout", "stderr", "returncode"]):
+        if check == "stdout":
+            assert ref.stdout == student.stdout, \
+                f"stdout differs:\n{diff(ref.stdout, student.stdout)}"
+        elif check == "stderr":
+            assert ref.stderr == student.stderr, \
+                f"stderr differs:\n{diff(ref.stderr, student.stderr)}"
+        elif check == "returncode":
+            assert ref.returncode == student.returncode, \
+                f"Exited with {student.returncode}, expected {ref.returncode}"
+        elif check == "has_stderr":
+            assert student.stderr != "", \
+                "The code should print an error message on stderr"
+
+def launch_test(binary, test_case):
+    try:
+        test(binary, test_case)
+    except AssertionError as err:
+        print(f"[{colored('KO', 'red')}]", test_case)
+        print(err)
+    else:
+        print(f"[{colored('OK', 'green')}]", test_case)
+
+def launch_tests(binary, categories, args):
+    """ Launch all tests found
+    find all tests in directories (or using categories given by user)
+    launch tests on binary taking into account arguments given
+    """
+    if not categories: # categories was not defined by the user
+        # Find all test files
+        tests_files = glob.glob("tests.yml", recursive=True)
+    else:
+        # find test files corresponding to categories
+        for category in categories:
+            tests_files.append(glob.glob(f"{category}*tests.yml", recursive=True))
+
+
+    # launch all tests of each file
+    for tests_file in tests_files:
+        with open(tests_file, "r") as tf:
+            print(f"{tests_file}\n")
+            test_list.append(yaml.safe_load(tf))
 
 if __name__ == "__main__":
 
@@ -27,5 +82,8 @@ if __name__ == "__main__":
 
     binary = Path(args.bin).absolute()
 
-    # launch tests on binary taking into account arguments given
-    launch_tests(binary, args)
+    # if list is activated, just show name of folders (categories)
+
+    # if sanity is activated, add option to binary
+
+    launch_tests(binary, args.categories, args)
