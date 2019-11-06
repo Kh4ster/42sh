f578555f24d80e49b0607bcac603c61a6777fbfb
pierrick.made <pierrick.made@epita.fr>
2019-11-06 21:40:56 +0100

    ADD - 42sh_tests.py - timeout feature working
    Signed-off-by: pierrick.made <pierrick.made@epita.fr>

--- tests/42sh_tests.py
+++ tests/42sh_tests.py
@@ -6,10 +6,13 @@ import yaml
 import os
 import subprocess as sp
 
-def run_shell(args, stdin):
+DEFAULT_TIMEOUT = 3
+
+def run_shell(args, stdin, timeout):
     """ Run the given args with stdin and return captured output """
 
-    return sp.run(args, capture_output=True, text=True, input=stdin)
+    return sp.run(args, capture_output=True, text=True, input=stdin,
+        timeout=timeout)
 
 def diff(ref, student):
     """ Return differences between input strings """
@@ -19,11 +22,14 @@ def diff(ref, student):
 
     return ''.join(unified_diff(ref, student, fromfile="ref", tofile="42sh"))
 
-def test(binary, test_case):
+def test(binary, test_case, timeout):
     """ run ref and student, check diff (return nothing or assertException) """
 
-    ref = run_shell(["bash", "--posix"], test_case["stdin"])
-    student = run_shell([binary], test_case["stdin"])
+    try:
+        ref = run_shell(["bash", "--posix"], test_case["stdin"], timeout)
+        student = run_shell([binary], test_case["stdin"], timeout)
+    except sp.TimeoutExpired:
+        raise AssertionError(f"Timeout after {timeout} seconds")
 
     for check in test_case.get("checks", ["stdout", "stderr", "returncode"]):
         if check == "stdout":
@@ -39,16 +45,31 @@ def test(binary, test_case):
             assert student.stderr != "", \
                 "The code should print an error message on stderr"
 
-def launch_test(binary, test_case):
+def launch_test(binary, test_case, args):
     """ Launch the test and print OK or KO (+diff) depending on the result """
 
+    # get timeout define in yaml or set DEFAULT_TIMEOUT
+    timeout = test_case.get("timeout", DEFAULT_TIMEOUT)
+    if args.timeout:
+        timeout = args.timeout
+
     try:
-        test(binary, test_case)
+        test(binary, test_case, timeout)
     except AssertionError as err:
         print(f"[{colored('KO', 'red')}]", test_case.get("name"))
         print(err)
     else:
         print(f"[{colored('OK', 'green')}]", test_case.get("name"))
+
+def launch_tests(binary, tests_files, args):
+    """ Launch all tests found in each test_file on binary """
+
+    # launch all tests of each file
+    for tests_file in tests_files:
+        with open(tests_file, "r") as tf:
+            print(f"{tests_file}")
+            for test_case in yaml.safe_load(tf):
+                launch_test(binary, test_case, args)
 
 def get_tests_files(categories):
     """ Find all tests in directories (or using categories given by user) """
@@ -64,16 +85,6 @@ def get_tests_files(categories):
             for found_test in Path(category).rglob('tests.yml'):
                 tests_files.append(found_test)
     return tests_files
-
-def launch_tests(binary, tests_files, args):
-    """ Launch all tests found in each test_file on binary """
-
-    # launch all tests of each file
-    for tests_file in tests_files:
-        with open(tests_file, "r") as tf:
-            print(f"{tests_file}")
-            for test_case in yaml.safe_load(tf):
-                launch_test(binary, test_case)
 
 def list_categories(tests_files):
     """ List all categories of tests """
@@ -99,8 +110,8 @@ if __name__ == "__main__":
     parser.add_argument("-s", "--sanity", action="store_true",
             dest="sanity", default=False,
             help="Execute the test suite with sanity checks enabled")
-    parser.add_argument("-t", "--timeout", action="store",
-            dest="time", default=0,
+    parser.add_argument("-t", "--timeout", type=int,
+            dest="timeout", default=0,
             help="Set time as a general timeout time (in seconds).")
     args = parser.parse_args()
 
