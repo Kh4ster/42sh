56f1c5b9601649f9b301cd303653fdb991c48417
pierrick.made <pierrick.made@epita.fr>
2019-11-07 00:02:49 +0100

    ADD - 42sh_tests.py - pretty print of synthesis with colors and global synthesis
    Signed-off-by: pierrick.made <pierrick.made@epita.fr>

--- tests/42sh_tests.py
+++ tests/42sh_tests.py
@@ -6,7 +6,7 @@ import yaml
 import os
 import subprocess as sp
 
-DEFAULT_TIMEOUT = 3
+DEFAULT_TIMEOUT = 2
 
 def run_shell(args, stdin, timeout):
     """ Run the given args with stdin and return captured output """
@@ -46,7 +46,10 @@ def test(binary, test_case, timeout):
                 "The code should print an error message on stderr"
 
 def launch_test(binary, test_case, args):
-    """ Launch the test and print OK or KO (+diff) depending on the result """
+    """ Launch the test and print OK or KO (+diff) depending on the result
+
+    return : 1 if test passed, 0 otherwise
+    """
 
     # get timeout define in yaml or set DEFAULT_TIMEOUT
     timeout = test_case.get("timeout", DEFAULT_TIMEOUT)
@@ -58,18 +61,42 @@ def launch_test(binary, test_case, args):
     except AssertionError as err:
         print(f"[{colored('KO', 'red')}]", test_case.get("name"))
         print(err)
-    else:
-        print(f"[{colored('OK', 'green')}]", test_case.get("name"))
+        return 0
+
+    print(f"[{colored('OK', 'green')}]", test_case.get("name"))
+    return 1
+
+def pretty_print_synthesis(passed, failed):
+    print(f"[{colored('==', 'red' if failed else 'blue')}] Synthesis: ", end='')
+    color_tested = colored(f"{passed + failed:2d}", 'blue')
+    print(f"Tested: {color_tested} | ", end='')
+    color_passed = colored (f"{passed:2d}", 'green')
+    print(f"Passing: {color_passed} | ", end='')
+    color_failed = colored (f"{failed:2d}", 'red' if failed else 'blue')
+    print(f"Failing: {color_failed}")
+
 
 def launch_tests(binary, tests_files, args):
-    """ Launch all tests found in each test_file on binary """
+    """ Launch with binary all tests found in each test_file """
 
-    # launch all tests of each file
+    total_passed, total_failed = 0, 0
+
     for tests_file in tests_files:
         with open(tests_file, "r") as tf:
-            print(f"{tests_file}")
+            print("\n" + f" {tests_file} ".center(80, "-") + "\n")
+            nb_passed, nb_failed = 0, 0
             for test_case in yaml.safe_load(tf):
-                launch_test(binary, test_case, args)
+                if launch_test(binary, test_case, args):
+                    nb_passed += 1
+                else:
+                    nb_failed += 1
+        total_passed += nb_passed
+        total_failed += nb_failed
+        pretty_print_synthesis(nb_passed, nb_failed)
+
+    print("\n" + " GLOBAL SYNTHESIS ".center(80, "-"))
+    pretty_print_synthesis(total_passed, total_failed)
+    print("".center(80, "-") + "\n")
 
 def get_tests_files(categories):
     """ Find all tests in directories (or using categories given by user) """
@@ -95,7 +122,7 @@ def list_categories(tests_files):
 
 
 if __name__ == "__main__":
-    """ This script launch automatically functional the testssuite"""
+    """ This script launch automatically the functional testssuite """
 
     # Set all arguments that can be used by the script
     parser = ArgumentParser(description="42sh Functional Testsuite")
