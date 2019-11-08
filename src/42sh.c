1a3692a5c5775ba288576f6c24d656443d2788d9
nicolas.blin <nicolas.blin@epita.fr>
2019-11-08 15:47:20 +0100

    [fix][42sh] use builtin next line instead of custom one

--- src/42sh.c
+++ src/42sh.c
@@ -1,13 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <err.h>
+#include <readline/readline.h>
+#include <unistd.h>
+#include <readline/history.h>
 
 #include "parameters_handling/parameters_handler.h"
 #include "parameters_handling/options.h"
 
-static void print_prompt(void)
+static int is_interactive(void)
 {
-    printf("42sh$ ");
+    int tty = rl_instream ? fileno(rl_instream) : fileno(stdin);
+
+    return isatty(tty);
+}
+
+static void prep_terminal(int meta_flag)
+{
+    if (is_interactive())
+        rl_prep_terminal(meta_flag);
+}
+
+static char *get_next_line(const char *prompt)
+{
+    rl_prep_term_function = prep_terminal;
+
+    if (!is_interactive())
+        prompt = NULL;
+
+    return readline(prompt);
+}
+
+static char *get_prompt(void)
+{
+    return "42sh$ ";
 }
 
 static void handle_line(char *line)
@@ -23,12 +49,9 @@ int main(int argc, char *argv[])
         return 2;
 
     char *line = NULL;
-    size_t read = 0;
-    print_prompt();
-    while (getline(&line, &read, stdin) != -1)
+    while ((line = get_next_line(get_prompt())) != NULL)
     {
         handle_line(line);
-        print_prompt();
     }
     return 0;
 }
