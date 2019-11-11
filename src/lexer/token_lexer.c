8f906e28fce274adcf7784776c76da31055a671d
pierrick.made <pierrick.made@epita.fr>
2019-11-11 23:25:52 +0100

    ADD - token_lexer.c - first ideas
    Signed-off-by: pierrick.made <pierrick.made@epita.fr>

--- src/lexer/token_lexer.c
+++ src/lexer/token_lexer.c
@@ -1,8 +1,25 @@
+#include <string.h>
+
 #include "../data_structures/queue.h"
 #include "token_lexer.h"
 
+void skip_class(int (*classifier)(int c), char **cursor)
+{
+    while (classifier(**cursor))
+        (*cursor)++;
+}
+
 struct queue *lexer(char *line)
 {
-    struct queue *new_queue = queue_init();
-    return new_queue;
+    struct queue *token_queue = queue_init();
+
+    char *cursor = line;
+
+    while (cursor != '\0')
+    {
+//     skip_class(isblank, &line);
+// find next_delimiter
+    }
+
+    return token_queue;
 }
