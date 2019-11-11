0f9542eca9a09f2a34f92b77394187afb0356d49
zakaria.ben-allal <zakaria.ben-allal@epita.fr>
2019-11-11 17:05:05 +0100

    [add] added informations to doxygen function comment

--- src/execution_handling/command_execution.h
+++ src/execution_handling/command_execution.h
@@ -1,7 +1,7 @@
 /** @file
 * @brief executes commands stored in command_container
 * @author Coder : zakaria.ben-allal
-* @author Tester :
+* @author Tester : zakaria.ben-allal
 * @author Reviewer :
 * @author Integrator :
 */
@@ -12,6 +12,10 @@
 * @brief Handle parameters given to 42sh binary
 * @param struct command_container
 * @param command_container struct that will hold the commands
-* @return
+* @return 0 if success
+* @return 127 if cmd not found
+* @return 126 if cmd not executable
+* @return 2 if cmd argument not valid
 */
+
 int exec_cmd(struct command_container *cmd);
