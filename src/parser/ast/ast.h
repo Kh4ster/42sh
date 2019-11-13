ca12a1de81bd27b9ee75384b265e653085b00182
cloe.lacombe <cloe.lacombe@epita.fr>
2019-11-13 18:10:15 +0000

    Removed anything relative to the old ast to match the new recursive parser

--- src/parser/ast/ast.h
+++ src/parser/ast/ast.h
@@ -9,19 +9,4 @@
 * @author Integrator :
 */
 
-struct data_ast {
-    enum token_parser_type type;
-    char *test;
-}
-
-
-struct ast {
-    struct data_ast *data;
-    struct ast *left_child;
-    struct ast *right_child;
-};
-
-
-extern struct ast *build_ast(struct stack *input);
-
 #endif
