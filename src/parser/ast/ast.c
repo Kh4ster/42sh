4f68ed5aad62393505dcbd158c974de44e040284
cloe.lacombe <cloe.lacombe@epita.fr>
2019-11-13 17:37:23 +0000

    Started the function to execute the ast
    ADD:
        ast.c: Create function execute_ast() wichj execute ewach node of the ast depending on
        a switch looking up the token type of the node. For now, only and_or type is handled.
        Next commit will handle if and commands.

--- src/parser/ast/ast.c
+++ src/parser/ast/ast.c
@@ -1,4 +1,42 @@
 #include "ast.h"
 #include "../parser.h"
+#include "../parser.h"
+
+static int (struct if_instruction *if_struct)
+{
+    
+}
 
 
+static int handle_and_or_instruction(struct instruction *ast)
+{
+    struct and_or_instruction *node = ast->data;
+
+    if (ast->type == OR)
+        return execute_ast(node->left) || execute_ast(node->right);
+
+    return execute_ast(node->left) && execute_ast(node->right);
+}
+
+
+static int handle_commands(struct instructions *ast)
+{
+    /* execute commande with zak function */
+    return 1;
+}
+
+
+extern int execute_ast(struct instruction *ast)
+{
+    switch (ast->type)
+    {
+        case TOKEN_OR:
+        case TOKEN_AND:
+            return handle_and_or_instruction(ast);
+            break;
+        case TOKEN_COMMAND:
+            return handle_commands(ast);
+            break;
+    }
+    return 1;
+}
