9afcab8ea0845f00aba80ca3591e990272b1ead4
cloe.lacombe <cloe.lacombe@epita.fr>
2019-11-14 18:14:22 +0000

    Started redirection from and to a file
    ADD:
        redirection.c: redirection.h: Created the structures of the future fonctions

---
+++ src/redirections_handling/redirect_left.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <sys/stat.h>
+
+#include "../execution_handling/redirector.c"
+#include "redirect_left.h"
+
+
+extern int redirect_stdin(char *file)
+
+
+extern int redirect_stdout(char *file)
+{
+     
+
+
+
+
---
+++ src/redirections_handling/redirect_left.h
@@ -0,0 +1,9 @@
+/** @file
+* @brief source file to handle the redirections from stdout to a file like this : "command" > file
+* @author Coder : cloe.lacombe
+* @author Tester :
+* @author Reviewer :
+* @author Integrator :
+*/
+
+#pragma once
