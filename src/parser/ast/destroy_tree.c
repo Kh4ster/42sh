f6a180ca3ecffde03b4ac504e4d1cb1fcae93990
cloe.lacombe <cloe.lacombe@epita.fr>
2019-11-14 17:38:50 +0000

    Removed compilation errors from destroy_tree.c

--- src/parser/ast/destroy_tree.c
+++ src/parser/ast/destroy_tree.c
@@ -1,7 +1,9 @@
+#include <stdlib.h>
+
 #include "ast.h"
 #include "../parser.h"
 #include "../../execution_handling/command_container.h"
-
+#include "destroy_tree.h"
 
 static void free_if (struct if_instruction *if_container)
 {
@@ -16,7 +18,7 @@ static void free_command(struct command_container *command)
     free(command->command);
 
     for (size_t i = 0; command->params[i]; i++)
-        free(command->params[i];
+        free(command->params[i]);
 
     free(command);
 }
@@ -27,18 +29,20 @@ extern void destroy_tree(struct instruction *ast)
     if (!ast)
         return;
 
+    struct and_or_instruction *node;
     switch (ast->type)
     {
         case TOKEN_OR:
         case TOKEN_AND:
-            destroy_tree(ast->data->left);
-            destroy_tree(ast->data->right);
+            node = ast->data;
+            destroy_tree(node->left);
+            destroy_tree(node->right);
             break;
         case TOKEN_COMMAND:
             free_command(ast->data);
             break;
         case TOKEN_IF:
-            return free_if(ast->data);
+            free_if(ast->data);
             break;
         default:
             return;
