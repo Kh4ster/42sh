237aa47bb6df98a1a24a16f372eff47e497b4a74
zakaria.ben-allal <zakaria.ben-allal@epita.fr>
2019-11-16 00:37:58 +0100

    [add] added functional tests for simple commands execution
    Signed-off-by: zakaria.ben-allal <zakaria.ben-allal@epita.fr>

---
+++ tests/week1_simplecommand/test3.sh
@@ -0,0 +1,25 @@
+echo 3
+mkdir toto
+cd toto
+touch toto
+mv toto papa
+rm papa
+cd ..
+rm -r toto
+echo 2
+mkdir toto
+cd toto
+touch toto
+mv toto papa
+rm papa
+cd ..
+rm -r toto
+echo 1
+mkdir toto
+cd toto
+touch toto
+mv toto papa
+rm papa
+cd ..
+rm -r toto
+echo toto
