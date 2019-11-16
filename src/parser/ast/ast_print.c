dc543797f17772bb9a89a1068e004b34972af965
cloe.lacombe <cloe.lacombe@epita.fr>
2019-11-16 00:43:14 +0000

    fixed the function print_ast to it prints correctly the ast
    ADD:
        print_ast: Added a labels system to have every if pointing where it is supposed to

--- src/parser/ast/ast_print.c
+++ src/parser/ast/ast_print.c
@@ -1,38 +1,79 @@
 #include <stdio.h>
 #include <err.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "ast.h"
 #include "../parser.h"
 #include "ast_print.h"
 #include "../../execution_handling/command_container.h"
 
+
+size_t nb_if = 1;
+size_t nb_cmd = 1;
+char *labels = "";
+
 static void __print_ast(struct instruction *ast, FILE *file);
+
 
 static void print_if_clause(struct if_instruction *if_clause, FILE *file)
 {
-    fprintf(file, "if -> ");
+    char *format_if = NULL;
+    asprintf(&format_if, "if_%ld", nb_if);
+    nb_if++;
+
+    fprintf(file, "%s -> ", format_if);
     __print_ast(if_clause->conditions, file);
     fprintf(file, " [label=\"conditions\"];\n");
-    fprintf(file, "if -> ");
-    __print_ast(if_clause->to_execute, file);
-    fprintf(file, " [label=\"then\"];\n");
+
+    if (if_clause->to_execute->type == TOKEN_IF)
+    {
+        fprintf(file, "%s -> if_%ld [label=then]\n", format_if, nb_if);
+        __print_ast(if_clause->to_execute, file);
+    }
+    else
+    {
+        fprintf(file, "%s -> ", format_if);
+        __print_ast(if_clause->to_execute, file);
+        fprintf(file, " [label=\"then\"];\n");
+    }
 
     if (if_clause->else_container)
     {
-        fprintf(file, "if -> ");
-        __print_ast(if_clause->else_container, file);
+        if (if_clause->to_execute->type == TOKEN_IF)
+        {
+            fprintf(file, "%s -> if_%ld [label=else]", format_if, nb_if);
+            __print_ast(if_clause->to_execute, file);
+        }
+        else
+        {
+        fprintf(file, "%s -> ", format_if);
+        __print_ast(if_clause->to_execute, file);
         fprintf(file, " [label=\"else\"];\n");
+        }
     }
+
+    asprintf(&labels, "%s%s [label=\"if\"];\n",labels, format_if);
+    free(format_if);
 }
 
 
 static void print_command(struct command_container *cmd, FILE *file)
 {
-    fprintf(file, "%s", cmd->command);
-/*
-    for (size_t i = 0; cmd->params[i]; i++)
-        fprintf(" %s", cmd->params[i], file);
-*/
+    char *format = NULL;
+    asprintf(&format, "%s_%ld",cmd->command, nb_cmd);
+    nb_cmd++;
+
+    char *label_cmd = NULL;
+    asprintf(&label_cmd, "%s", cmd->command);
+
+    for (size_t i = 1; cmd->params[i]; i++)
+        asprintf(&label_cmd,"%s %s", label_cmd, cmd->params[i]);
+
+    fprintf(file, "%s", format);
+    asprintf(&labels, "%s%s [label=\"%s\"];\n",labels, format, label_cmd);
+    free(format);
+    free(label_cmd);
 }
 
 
@@ -100,7 +141,9 @@ extern void print_ast(struct instruction *ast)
     if (!dot_ast)
         warn("Could not open file ast.dot");
 
-    fprintf(dot_ast, "diagraph ast {\n");
+    fprintf(dot_ast, "digraph ast {\n");
     __print_ast(ast, dot_ast);
+    fprintf(dot_ast, "%s", labels);
     fprintf(dot_ast, "}\n");
+    fclose(dot_ast);
 }
