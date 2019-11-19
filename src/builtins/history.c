483ce30740d06d844e7e62ceb9fce6c1c30aec4d
pierrick.made <pierrick.made@epita.fr>
2019-11-19 19:37:38 +0100

    ADD - history.c - draft, not finished
    Signed-off-by: pierrick.made <pierrick.made@epita.fr>

--- src/builtins/history.c
+++ src/builtins/history.c
@@ -2,23 +2,56 @@
 
 #include "history.h"
 
+static int is_number(char *data)
+{
+    for (size_t i = 0; data[i]; i++)
+    {
+        if (data[i] < '0' || data[i] > '9')
+            return 0;
+    }
+
+    return 1;
+}
+
 int history(struct array_list *args)
 {
-    // handle options
-    int r_opt = 0;
-    int c_opt = 0;
-    for (size_t i = 0; i < args->nb_element; i++)
+    if (args->nb_element == 0)
     {
-        char *arg = args->content[i];
-        if (arg[0] == '-')
+        // return : display all history file
+    }
+    char *first_arg = args->content[0];
+    if (first_arg[1] == '-')
+    {
+        // handle options
+        int r_opt = 0;
+        int c_opt = 0;
+        for (size_t i = 0; i < args->nb_element; i++)
         {
-            if (arg[1] == 'c')
-                c_opt = 1;
-            else if (arg[1] == 'r')
-                r_opt = 1;
-            else
-                err("history: -%c: invalid option", arg[1]);
+            char *arg = args->content[i];
+            if (arg[0] == '-')
+            {
+                if (arg[1] == 'c')
+                    c_opt = 1;
+                else if (arg[1] == 'r')
+                    r_opt = 1;
+                else
+                    err(99, "history: -%c: invalid option", arg[1]);
+            }
         }
-        if (is_num_arg)
+        if (c_opt)
+            //history_clear()
+        else if (r_opt)
+            //history_restore()
+    }
+    else if (is_number(first_arg))
+    {
+        if (args->nb_element == 1)
+        {
+            // return : display n lines of history file
+        }
+        else
+        {
+            /* code */
+        }
     }
 }
