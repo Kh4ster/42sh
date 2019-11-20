a897d9164ec5283440a522b0474a274d67c38073
nicolas.blin <nicolas.blin@epita.fr>
2019-11-20 14:06:30 +0100

    [refactor] remove useless function, remove useless call to save std

--- src/execution_handling/redirector.c
+++ src/execution_handling/redirector.c
@@ -29,38 +29,3 @@ extern int save_one_fd(int fd_save, int fd)
 
     return 0;
 }
-
-/*
-* Copy stdin/out/err to 10/11/12
-* Allows us to then modify the stds of the bins we want to execute
-* And then get back the true stds
-* return -1 in case of error
-*/
-
-int restore_stds(void)
-{
-    int errors[3];
-    errors[0] = dup2(10, 0);
-    errors[1] = dup2(11, 1);
-    errors[2] = dup2(12, 2);
-    for (short i = 0; i < 3; i++)
-    {
-        if (errors[i] == -1)
-            return -1;
-    }
-    return 0;
-}
-
-int save_stds(void)
-{
-    int errors[3];
-    errors[0] = dup2(0, 10);
-    errors[1] = dup2(1, 11);
-    errors[2] = dup2(2, 12);
-    for (short i = 0; i < 3; i++)
-    {
-        if (errors[i] == -1)
-            return -1;
-    }
-    return 0;
-}
