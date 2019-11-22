3db55d20b53bb5ce82f968ab9df8a86a2bee3614
pierrick.made <pierrick.made@epita.fr>
2019-11-22 20:50:51 +0100

    ADD - 42sh.c - history feature + refactor coding style
    Signed-off-by: pierrick.made <pierrick.made@epita.fr>

--- src/42sh.c
+++ src/42sh.c
@@ -91,6 +91,17 @@ void free_all(struct queue *lexer)
     hash_free(g_env.functions);
     hash_free(g_env.builtins);
     free(lexer);
+
+    // History write to file
+    char *history_path = get_history_file_path();
+    FILE *history_file = fopen(history_path, "w");
+    if (history_file == NULL)
+        return;
+    HIST_ENTRY **hist_elts = history_list();
+    for (int i = 0; i < history_length - 1; i++)
+        fprintf(history_file, "%s\n", hist_elts[i]->line);
+
+    free(history_path);
 }
 
 static void init_builtins_hash_map(struct hash_map *builtins)
@@ -100,14 +111,35 @@ static void init_builtins_hash_map(struct hash_map *builtins)
     hash_insert_builtin(builtins, "history", history);
 }
 
-static void init_hash_maps(struct hash_map *functions,
+static void init_hash_maps_and_history(struct hash_map *functions,
                             struct hash_map *builtins
 )
 {
+    // Hash map
     g_env.functions = functions;
     g_env.builtins = builtins;
     hash_init(functions, NB_SLOTS);
     init_builtins_hash_map(g_env.builtins);
+
+    // History
+    char *history_path = get_history_file_path();
+    using_history();
+    read_history(history_path);
+    free(history_path);
+}
+
+static void handle_signal(void)
+{
+    if (signal(SIGINT, sigint_handler) == SIG_ERR)
+        errx(1, "an error occurred while setting up a signal handler");
+}
+
+static int execute_and_print_ast(struct instruction *ast)
+{
+    if (g_env.options.option_a)
+        print_ast(ast);
+
+    return execute_ast(ast);
 }
 
 int main(int argc, char *argv[])
@@ -115,43 +147,35 @@ int main(int argc, char *argv[])
     if (handle_parameters(&g_env.options, argc, argv) == -1)
         errx(2, "invalid option or file");
 
-    if (signal(SIGINT, sigint_handler) == SIG_ERR)
-        errx(1, "an error occurred while setting up a signal handler");
+    handle_signal();
 
     struct hash_map functions; //declared on the stack no need to be freed
     struct hash_map builtins;
-    init_hash_maps(&functions, &builtins);
+    init_hash_maps_and_history(&functions, &builtins);
 
     handle_ressource_files();
 
     int is_end = 0;
     struct queue *lexer = queue_init();
-    int error = 0;
     int return_code = 0;
 
     while (42)
     {
+        int error = 0;
         g_env.prompt = "42sh$ ";
         struct instruction *ast = parse_input(lexer, &is_end, &error);
 
-        if (g_env.options.option_a)
-            print_ast(ast);
-
-        return_code = execute_ast(ast);
+        return_code = execute_and_print_ast(ast);
 
         destroy_tree(ast);
 
-        if (signal(SIGINT, sigint_handler) == SIG_ERR)
-            errx(1, "an error occurred while setting up a signal handler");
+        handle_signal();
 
         if (is_end)
             break;
 
         else if (error)
-        {
             handle_parser_errors(lexer);
-            error = 0; //set error back to 0 for interactive mode
-        }
     }
 
     free_all(lexer);
