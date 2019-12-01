374b31b9983adb647c0345908c438f8ec9ce26c8
nicolas.blin <nicolas.blin@epita.fr>
2019-12-01 04:02:29 +0100

    [fix][j'ai fixé le chapeau] need to -1 cause ++ in for make is go forwak by one

--- src/parser/ast/ast.c
+++ src/parser/ast/ast.c
@@ -119,7 +119,7 @@ static char *le_chapeau_de_expand_cmd(char **to_expand,
     char *beg = strdup(*to_expand);
     char *end_parenthesis = beg + nb_to_skip;
     find_corresponding_parenthesis(&end_parenthesis, NULL);
-    *to_expand += (end_parenthesis - beg);
+    *to_expand += (end_parenthesis - beg - 1);
     *end_parenthesis = '\0';
     char *result = expand_cmd(beg, to_stop, nb_to_skip);
     free(beg);
